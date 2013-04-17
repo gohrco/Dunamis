4d4cf597d4b385d3872f9c2f09d1cdc3b8aed114
steven_gohigher <steven_gohigher@42d893b8-9b43-4f68-b6d9-13947b35104d>
2013-04-17 15:30:29 +0000

    *  Input handler modified

--- branches/1.1-Joomla/dunamis/joomla/input.php
+++ branches/1.1-Joomla/dunamis/joomla/input.php
@@ -38,6 +38,26 @@ class JoomlaDunInput extends DunInput
 	
 	
 	/**
+	 * Method to return what method we are using to view the page
+	 * @access		public
+	 * @version		@fileVers@ ( $id$ )
+	 *
+	 * @return		string
+	 * @since		1.1.0
+	 */
+	public function getMethod()
+	{
+		if ( version_compare( JVERSION, '1.7.0', 'ge' ) ) {
+			$app	= & JFactory :: getApplication();
+			return strtolower( $app->input->getMethod() );
+		}
+		else {
+			return strtolower( JRequest :: getMethod() );
+		}
+	}
+	
+	
+	/**
 	 * Method to load the input into the object
 	 * @access		protected
 	 * @version		@fileVers@
@@ -50,17 +70,19 @@ class JoomlaDunInput extends DunInput
 		$get = $post = $request = array();
 		
 		$app	= & JFactory :: getApplication();
-		$cycle	=	array( 'get' => $_GET, 'post' => $_POST, 'request' => $_REQUEST );
+		$cycle	=	array( 'get' => $_GET, 'post' => $_POST, 'request' => $_REQUEST, 'server' => $_SERVER );
 		$data	=	array( 'get' => array(), 'post' => array(), 'request' => array() );
 		
 		foreach ( $cycle as $k => $v ) {
 			foreach ( $v as $f => $trash ) {
 				
+				$t	=	is_array( $trash ) ? 'array' : 'string';
+				
 				if ( version_compare( DUN_ENV_VERSION, '1.7.1', 'ge' ) ) {
-					$data[$k][$f] = $app->input->$k->get( $f );
+					$data[$k][$f] = $app->input->$k->get( $f, null, $t );
 				}
 				else {
-					$data[$k][$f]	=	JRequest :: getVar( $f, null, $k );
+					$data[$k][$f]	=	JRequest :: getVar( $f, null, $k, $t );
 				}
 			}
 		}
