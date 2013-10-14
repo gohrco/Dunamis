98b94303f01d1ba28a09d471fc8e5351d2c3dde7
steven_gohigher <steven_gohigher@42d893b8-9b43-4f68-b6d9-13947b35104d>
2013-10-14 02:56:21 +0000

    *  Bamboo Server Work

---
+++ branches/1.2/tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+//defined( 'WHMCS' ) or define( 'WHMCS', true );
+
+chdir( 'c:\xampp\www\mods\whmcs' );
+//echo "\n\n" . getcwd()."\n\n"; die();
+
+error_reporting(0);
+include 'init.php';
+/*
+error_reporting(E_ALL);
+var_dump( include "/includes/classes/class.init.php" ); die();
+
+$whmcs = new WHMCS_Init();
+$whmcs = $whmcs->init();
+
+global $CONFIG;
+$CONFIG	=	array(
+		'Version'	=>	'5.2.8',
+		);
+
+
+*/
