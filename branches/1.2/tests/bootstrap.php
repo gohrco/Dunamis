48234343a95ed066dd6a438ab265a8f2ab640999
steven_gohigher <steven_gohigher@42d893b8-9b43-4f68-b6d9-13947b35104d>
2013-10-15 14:23:20 +0000

    *  Bamboo Server Work

--- branches/1.2/tests/bootstrap.php
+++ branches/1.2/tests/bootstrap.php
@@ -2,27 +2,48 @@
 
 defined( 'WHMCS' ) or define( 'WHMCS', true );
 
+$ds	=	 DIRECTORY_SEPARATOR;
+
 // Bamboo testing
 if ( isset( $_ENV['bamboo'] ) && $_ENV['bamboo'] == 'true' ) {
 	require_once '/home/jwhmcsco/public_html/hosting/includes/dunamis.php';
+	require_once '/home/jwhmcsco/public_html/hosting/includes/dbfunctions.php';
+	require_once '/home/jwhmcsco/public_html/hosting/configuration.php';
+	$rootpath	=	'/home/jwhmcsco/public_html/hosting' . $ds;
 }
 else {
 	require_once 'C:\xampp\www\mods\whmcs\includes\dunamis.php';
+	require_once 'C:\xampp\www\mods\whmcs\includes\dbfunctions.php';
+	require_once 'C:\xampp\www\mods\whmcs\configuration.php';
+	$rootpath	=	'C:\xampp\www\mods\whmcs' . $ds;
 }
 
+/* ----------------------- */
+/* Connect to our database */
+/* ----------------------- */
+global $whmcsmysql;
+$whmcsmysql = mysql_connect( $db_name, $db_username, $db_password );
+mysql_select_db( $db_name );
+/* ----------------------- */
+/* End Connect to database */
+/* ----------------------- */
+
+/* Register an autoloader */
 spl_autoload_register( function ( $class )  {
 	
-	require_once '/home/jwhmcsco/public_html/hosting/includes/dunamis/core/object.php';
+	require_once $rootpath . 'includes' . $ds . 'dunamis' . $ds . 'core' . $ds . 'object.php';
 	
-	if ( strpos( $class, 'Dun' ) ) {
-		require_once '/home/jwhmcsco/public_html/hosting/includes/dunamis/core/' . strtolower( str_replace( 'Dun', '', $class ) ) . '.php';
+	if ( strpos( $class, 'Dun' ) !== false ) {
+		require_once $rootpath . 'includes' . $ds . 'dunamis' . $ds . 'core' . $ds . strtolower( str_replace( 'Dun', '', $class ) ) . '.php';
 	}
-	else if ( strpos( $class, 'Whmcs' ) ) {
-		$base	=	'/home/jwhmcsco/public_html/hosting/includes/dunamis/core/' . strtolower( str_replace( 'Whmcs', '', $class ) ) . '.php';
+	else if ( strpos( $class, 'Whmcs' ) !== false ) {
+		$base	=	$rootpath . 'includes' . $ds . 'dunamis' . $ds . 'core' . $ds . strtolower( str_replace( 'Whmcs', '', $class ) ) . '.php';
 		if ( file_exists( $base ) ) {
 			require_once $base;
 		}
-		require_once '/home/jwhmcsco/public_html/hosting/includes/dunamis/whmcs/' . strtolower( str_replace( 'Whmcs', '', $class ) ) . '.php';
+		else {
+			require_once $rootpath . 'includes' . $ds . 'dunamis' . $ds . 'whmcs' . $ds . strtolower( str_replace( 'Dun', '', $class ) ) . '.php';
+		}
 	}
 	
 });
