93d817d4a91594b2eeccada489a2f342b9a0f291
Steven Mueller <gohrco@users.noreply.github.com>
2015-07-08 14:10:45 +0000

    #  [JOOMLA] Cleanup of legacy code for 2.5 failing to load pages properly

--- joomla/component/admin/dunamis.legacy.php
+++ joomla/component/admin/dunamis.legacy.php
@@ -35,20 +35,11 @@ else if ( version_compare( JVERSION, '1.6', 'ge' ) )
 	jimport('joomla.application.component.controller');
 	jimport('joomla.application.component.controllerform');
 	jimport('joomla.application.component.model');
-
-	// Good ol' Joomla changing things up mid-stream
-	if ( version_compare( JVERSION, '2.5.5', 'ge' ) ) {
-		jimport( 'cms.view.legacy' );
-		if (! class_exists( 'DunamisViewExt' ) ) {
-			class DunamisViewExt extends JViewLegacy {}
-		}
-	} else {
-		jimport( 'joomla.application.component.view' );
-		if (! class_exists( 'DunamisViewExt' ) ) {
-			class DunamisViewExt extends JView {}
-		}
+	jimport( 'joomla.application.component.view' );
+	
+	if (! class_exists( 'DunamisViewExt' ) ) {
+		class DunamisViewExt extends JView {}
 	}
-
 	if (! class_exists( 'DunamisControllerExt' ) ) {
 		class DunamisControllerExt extends JController {}
 	}
