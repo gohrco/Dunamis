00fb80995dcd46f111ebd1aff58f1be0ab247eca
steven_gohigher <steven_gohigher@42d893b8-9b43-4f68-b6d9-13947b35104d>
2015-12-03 23:12:30 +0000

    Dunamis Framework version 1.4.5 tagged

---
+++ tags/1.4.5/joomla/component/admin/dunamis.legacy.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @projectName@
+ * Joomla! - Legacy Handler
+ *
+ * @package    @projectName@
+ * @copyright  @copyWrite@
+ * @license    @buildLicense@
+ * @version    @fileVers@ ( $Id$ )
+ * @author     @buildAuthor@
+ * @since      3.1.00
+ *
+ * @desc       This file permits Dunamis to operate across Joomla! versions
+ *
+ */
+
+
+if ( version_compare( JVERSION, '3.0', 'ge' ) )
+{
+	if (! class_exists( 'DunamisControllerExt' ) ) {
+		class DunamisControllerExt extends JControllerLegacy {}
+	}
+	if (! class_exists( 'DunamisControllerForm' ) ) {
+		class DunamisControllerForm extends JControllerForm {}
+	}
+	if (! class_exists( 'DunamisModelExt' ) ) {
+		class DunamisModelExt extends JModelLegacy {}
+	}
+	if (! class_exists( 'DunamisViewExt' ) ) {
+		class DunamisViewExt extends JViewLegacy {}
+	}
+}
+else if ( version_compare( JVERSION, '1.6', 'ge' ) )
+{
+	jimport('joomla.application.component.controller');
+	jimport('joomla.application.component.controllerform');
+	jimport('joomla.application.component.model');
+	jimport( 'joomla.application.component.view' );
+	
+	if (! class_exists( 'DunamisViewExt' ) ) {
+		class DunamisViewExt extends JView {}
+	}
+	if (! class_exists( 'DunamisControllerExt' ) ) {
+		class DunamisControllerExt extends JController {}
+	}
+	if (! class_exists( 'DunamisControllerForm' ) ) {
+		class DunamisControllerForm extends JControllerForm {}
+	}
+	if (! class_exists( 'DunamisModelExt' ) ) {
+		class DunamisModelExt extends JModel {}
+	}
+}
+else
+{
+	jimport('joomla.application.component.controller');
+	jimport('joomla.application.component.model');
+	jimport( 'joomla.application.component.view' );
+
+	if (! class_exists( 'DunamisControllerExt' ) ) {
+		class DunamisControllerExt extends JController {}
+	}
+	if (! class_exists( 'DunamisControllerForm' ) ) {
+		class DunamisControllerForm extends JController {}
+	}
+	if (! class_exists( 'DunamisModelExt' ) ) {
+		class DunamisModelExt extends JModel {}
+	}
+	if (! class_exists( 'DunamisViewExt' ) ) {
+		class DunamisViewExt extends JView {}
+	}
+}
